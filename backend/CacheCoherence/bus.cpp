565020b4ff6275660c3068e70775d5fcc101d79f
Rachel B. Yanovsky <ryanovsk@unix1.andrew.cmu.edu>
2016-04-22 14:50:28 -0400

    fixed shared variable update

--- backend/CacheCoherence/bus.cpp
+++ backend/CacheCoherence/bus.cpp
@@ -19,10 +19,10 @@ int Bus::sendMsgToBus(int core_num, request_t request, uint64_t addr){
       //update the status of the cache based on the message on the bus
       if(caches[i]->updateStatus(request, addr)){
         dirty = true;
-        shared = shared || caches[i]->checkState(addr) == SHARED;
         mem->flush();
         dirty = false;
       }
+        shared = shared || (caches[i]->checkState(addr) == SHARED);
     }
   }
   snoop_pending = false;
