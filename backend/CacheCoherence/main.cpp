2de471e54a7eb62733936addd8dfbb8f77579297
Ashwin Raghavachari <ashwinr@unix3.andrew.cmu.edu>
2016-05-08 21:08:53 -0400

    bad inputs protected

--- backend/CacheCoherence/main.cpp
+++ backend/CacheCoherence/main.cpp
@@ -3,9 +3,10 @@
 
 int main(int argc, char** argv)
 {
-  if (argc != 3)
+  if ((argc != 3) || atoi(argv[1]) < 8)
   {
     fprintf(stderr, "%s <size of cache, log2> <taskgraph>\n", argv[0]);
+    fprintf(stderr, "size of cache must be greater than 7!\n");
     return 1;
   }
 
